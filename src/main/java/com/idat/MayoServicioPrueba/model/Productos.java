0a7b21ab327963b3980d253100a79c5bd3e587aa
eddiejhp <eddiejhp@gmail.com>
2022-06-14 19:23:46 -0500

    Conexion relacion tablas

--- src/main/java/com/idat/MayoServicioPrueba/model/Productos.java
+++ src/main/java/com/idat/MayoServicioPrueba/model/Productos.java
@@ -1,9 +1,19 @@
 package com.idat.MayoServicioPrueba.model;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Table(name = "Productos") // Por buenas practicas definir el nombre de la tabla
@@ -12,12 +22,34 @@ public class Productos {
 
 	@Id //Para indicar que es el identificador y Primary Key
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //Para indicar que el campo sea autogenerado 1 2 3...
-	private Integer idProducto;
-	//@Column(definir nombre de la tabla) Opcional
+	private Integer idProducto; //@Column(definir nombre de la tabla) Opcional
 	private String nombreProducto;
 	private String descripcion;
 	private Double precio;
 	private Integer stock;
+	
+	//Para relacionar 1 a 1
+	@OneToOne (mappedBy = "producto") //Se coloca el cualquiera de las clases solo cuando ese de 1 a 1 o muchos a muchos
+	private Proveedor proveedor;
+	
+	@ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+	@JoinTable(
+			name = "productos_clientes",
+			joinColumns = @JoinColumn(
+					name = "id_producto", 
+					nullable = false, 
+					unique =true, 
+					foreignKey =  @ForeignKey(foreignKeyDefinition = "foreign key(id_producto) references Productos(id_producto)")					
+			),
+			inverseJoinColumns = @JoinColumn(
+					name = "id_Cliente", 
+					nullable = false, 
+					unique =true, 
+					foreignKey =  @ForeignKey(foreignKeyDefinition ="foreign key(id_cliente) references cliente(id_cliente)")						
+					)
+			)
+	
+	private List<Cliente> cliente = new ArrayList<>();
 	
 	
 	public Integer getIdProducto() {
